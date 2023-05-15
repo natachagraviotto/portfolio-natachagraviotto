89b71a5c048edcba217df1c4ebf1fea6c47be53c
natachagraviotto <116673804+natachagraviotto@users.noreply.github.com>
2023-05-15 18:30:58 +0200

    delete error folder and link menu

--- src/App.jsx
+++ src/App.jsx
@@ -8,13 +8,16 @@ function App() {
       <header className="menu">
         <strong className="logotipo">MI LOGO</strong>
         <nav className="navegation">
-          <Link className="link" to="/list">
+          <Link className="link" to="/">
+            Home
+          </Link>
+          <Link className="link" to="/projects">
             Projects
           </Link>
-          <Link className="link" to="/list">
+          <Link className="link" to="/about">
             About
           </Link>
-          <Link className="link" to="/list">
+          <Link className="link" to="/contact">
             Contact
           </Link>
         </nav>
