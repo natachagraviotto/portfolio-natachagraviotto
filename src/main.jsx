2e8bc325bfd62840ea80c2a2c0df3533e42bd505
natachagraviotto <116673804+natachagraviotto@users.noreply.github.com>
2023-06-06 19:37:26 +0200

    import logo and link welcome page

--- src/main.jsx
+++ src/main.jsx
@@ -8,6 +8,18 @@ import Projects from "./views/projects/Projects.jsx";
 import Landing from "./views/landing/Landing.jsx";
 import About from "./views/about/About.jsx";
 import Contact from "./views/contact/Contact.jsx";
+import Welcome from "./views/welcome/Welcome.jsx";
+import { Link } from "react-router-dom";
+
+const Logo = () => {
+  return (
+    <div className="logotipo-negro">
+      <Link to="/welcome">Welcome</Link>
+    </div>
+  );
+};
+
+export default Logo;
 
 const router = createBrowserRouter([
   {
@@ -15,7 +27,8 @@ const router = createBrowserRouter([
     element: <App />,
     errorElement: <div>Esto está roto</div>,
     children: [
-      { index: true, element: <Landing /> },
+      { index: true, element: <Welcome /> },
+      { path: "landing", element: <Landing /> },
       { path: "projects", element: <Projects /> },
       { path: "about", element: <About /> },
       { path: "contact", element: <Contact /> },
