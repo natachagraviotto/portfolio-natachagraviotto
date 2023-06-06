271f6aa583d03151740cc03d1d13a98aea952a00
natachagraviotto <116673804+natachagraviotto@users.noreply.github.com>
2023-06-06 19:40:02 +0200

    icon upload

--- src/components/content-link/ContentLink.jsx
+++ src/components/content-link/ContentLink.jsx
@@ -1,11 +1,15 @@
 import { Link } from "react-router-dom";
+import IconArrowReturnRight from "../icons/IconArrow";
 
 function ContentLink(props) {
   return (
-    <Link className="home-button" to={props.to}>
-      {props.children}
-    </Link>
+    <>
+      <IconArrowReturnRight />
+      <Link className="home-button" to={props.to}>
+        {props.children}
+        {props.icon}
+      </Link>
+    </>
   );
 }
-
 export default ContentLink;
---
+++ src/components/icons/IconArrow.jsx
@@ -0,0 +1,22 @@
+// icon:arrow-return-right | Bootstrap https://icons.getbootstrap.com/ | Bootstrap
+import * as React from "react";
+
+function IconArrowReturnRight(props) {
+  return (
+    <svg
+      width="32"
+      height="35"
+      viewBox="0 0 32 35"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      className={`svgIcon ${props.color}`}
+    >
+      <path
+        d="M32 24L21 35L18.16 32.18L24.34 26H13C5.82 26 0 20.18 0 13V0H4V13C4 18 8 22 13 22H24.34L18.18 15.82L21 13L32 24Z"
+        fill="black"
+      />
+    </svg>
+  );
+}
+
+export default IconArrowReturnRight;
