0c0b0d7ba85460dbaa8acb2154d3a405669b3b5c
natachagraviotto <116673804+natachagraviotto@users.noreply.github.com>
2023-06-11 22:05:24 +0200

    change children property to text

--- src/views/landing/Landing.jsx
+++ src/views/landing/Landing.jsx
@@ -4,13 +4,11 @@ import "./landing.scss";
 function Landing() {
   return (
     <section className="home-page">
-      <h2 className="home-h2">Heeey, I’m Natacha Graviotto</h2>
-      <h1 className="home-h1">
-        I’m a UX/UI designer, graphic designer & front-end developer
-      </h1>
-      <div className="box-buttons">
-        <ContentLink to="/projects" children="See my projects" icon="" />
-        <ContentLink to="/contact">Contact me :)</ContentLink>
+      <div className="title">Heeey, I’m Natacha Graviotto</div>
+      <h1>I’m a UX/UI designer, graphic designer & front-end developer</h1>
+      <div className="box">
+        <ContentLink to="/projects" text="See my projects" icon="" />
+        <ContentLink to="/projects" text="Contact me" icon="" />
       </div>
     </section>
   );
