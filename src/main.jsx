0acb5adad7edaf097f847cd918ed24a9cf881fac
natachagraviotto <116673804+natachagraviotto@users.noreply.github.com>
2023-06-13 10:59:37 +0200

    modification of project page links

--- src/main.jsx
+++ src/main.jsx
@@ -10,6 +10,7 @@ import About from "./views/about/About.jsx";
 import Contact from "./views/contact/Contact.jsx";
 import Welcome from "./views/welcome/Welcome.jsx";
 import { Link } from "react-router-dom";
+import ProjectDetail from "./views/project-detail/ProjectDetail.jsx";
 
 const Logo = () => {
   return (
@@ -32,6 +33,94 @@ const router = createBrowserRouter([
       { path: "projects", element: <Projects /> },
       { path: "about", element: <About /> },
       { path: "contact", element: <Contact /> },
+      {
+        path: "projects/viro",
+        element: (
+          <ProjectDetail
+            category="Brand Identity Design"
+            tag="Ilustrator/Photoshop"
+            color="bisque"
+            name="VIRO"
+            github="https://github.com/natachagraviotto/portfolio-natachagraviotto"
+            text="This project highlights his unique style and creative approach, reflecting his artistic vision and technical skills. It conveys sophistication and elegance establishing a solid foundation to promote his brand and attract new clients in the world of interior design and architecture."
+            images={["/img/project-viro.jpg", "/img/project-viro.jpg"]}
+          />
+        ),
+      },
+      {
+        path: "projects/app",
+        element: (
+          <ProjectDetail
+            category="Prototipe App"
+            tag="Adobe XD"
+            color="pink"
+            name="APP HER"
+            behance="https://www.behance.net/gallery/167550531/App-menstrual-Her"
+            text="This project highlights his unique style and creative approach, reflecting his artistic vision and technical skills. It conveys sophistication and elegance establishing a solid foundation to promote his brand and attract new clients in the world of interior design and architecture."
+            images={["/img/project-viro.jpg", "/img/project-viro.jpg"]}
+          />
+        ),
+      },
+      {
+        path: "projects/project3",
+        element: (
+          <ProjectDetail
+            tag="Ilustrator/Photoshop"
+            title="project 3"
+            github="https://www.github.com"
+            text="`${projectAppText}`"
+            images={[
+              "https://dummyimage.com/600x400/000/fff",
+              "https://dummyimage.com/600x400/000/ff0000",
+            ]}
+          />
+        ),
+      },
+      {
+        path: "projects/project4",
+        element: (
+          <ProjectDetail
+            tag="Ilustrator/Photoshop"
+            title="project 4"
+            github="https://www.github.com"
+            text="`${projectAppText}`"
+            images={[
+              "https://dummyimage.com/600x400/000/fff",
+              "https://dummyimage.com/600x400/000/ff0000",
+            ]}
+          />
+        ),
+      },
+      {
+        path: "projects/project4",
+        element: (
+          <ProjectDetail
+            tag="Ilustrator/Photoshop"
+            title="project 4"
+            github="https://www.github.com"
+            text="`${projectAppText}`"
+            images={[
+              "https://dummyimage.com/600x400/000/fff",
+              "https://dummyimage.com/600x400/000/ff0000",
+            ]}
+          />
+        ),
+      },
+      {
+        path: "projects/project4",
+        element: (
+          <ProjectDetail
+            tag="Ilustrator/Photoshop"
+            title="project 4"
+            github="https://www.github.com"
+            text="`${projectAppText}`"
+            images={[
+              "https://dummyimage.com/600x400/000/fff",
+              "https://dummyimage.com/600x400/000/ff0000",
+            ]}
+          />
+        ),
+      },
     ],
   },
 ]);
--- src/views/projects/Projects.jsx
+++ src/views/projects/Projects.jsx
@@ -6,40 +6,44 @@ function Projects() {
     <section className="hero-projects">
       <h1>Projects</h1>
       <div className="carrusel-projects">
-        <ProjectBox title="project1" color="bisque" bgclass="bg-viro" />
-        <ProjectBox title="project2" color="pink" bgclass="bg-app" />
+        <ProjectBox
+          title="Brand Identity Design"
+          color="bisque"
+          bgclass="bg-viro"
+          link="/projects/viro"
+        />
+        <ProjectBox
+          title="Prototipado App her"
+          color="pink"
+          link="/projects/app"
+          bgclass="bg-app"
+        />
 
-        <div className="box-project">
-          <button className="box-button box-button-pink">
-            Brand Identity Design
-          </button>
-        </div>
+        <ProjectBox
+          title="Calculator project"
+          color="pink"
+          link="/projects/calculator"
+          bgclass="bg-calculator"
+        />
+        <ProjectBox
+          title="Brand Identity Design"
+          color="bisque"
+          bgclass="bg-viro"
+          link="/projects/viro"
+        />
+        <ProjectBox
+          title="Prototipado App her"
+          color="pink"
+          link="/projects/app"
+          bgclass="bg-app"
+        />
 
-        <div className="box-project">
-          <button className="box-button box-button-bisque">
-            Brand Identity Design
-          </button>
-        </div>
-        <div className="box-project">
-          <button className="box-button box-button-lightgreen">
-            Brand Identity Design
-          </button>
-        </div>
-        <div className="box-project">
-          <button className="box-button box-button-lightyellow">
-            Brand Identity Design
-          </button>
-        </div>
-        <div className="box-project">
-          <button className="box-button box-button-lightcoral">
-            Brand Identity Design
-          </button>
-        </div>
-        <div className="box-project">
-          <button className="box-button box-button-lightseagreen">
-            Brand Identity Design
-          </button>
-        </div>
+        <ProjectBox
+          title="Calculator project"
+          color="pink"
+          link="/projects/calculator"
+          bgclass="bg-calculator"
+        />
       </div>
     </section>
   );
