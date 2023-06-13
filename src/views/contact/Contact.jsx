5d372392850ae9050e636bf1af2a5b73b7716c4e
natachagraviotto <116673804+natachagraviotto@users.noreply.github.com>
2023-06-13 23:28:26 +0200

    fix styles

--- src/views/contact/Contact.jsx
+++ src/views/contact/Contact.jsx
@@ -1,4 +1,8 @@
+import { Link } from "react-router-dom";
 import ContentLink from "../../components/content-link/ContentLink";
+import "./contact.scss";
+import React from "react";
+//import Obfuscate from "react-obfuscate";
 
 function Contact() {
   return (
@@ -6,15 +10,37 @@ function Contact() {
       <h1>Contact me! :)</h1>
       <div className="contact-mail">
         <h3 className="contact-text">Mail</h3>
-        <p>ngraviotto@gmail.com</p>
+        <p className="email">
+          {/* <a href="mailto:n" */}
+          <Link to="mailto:ngraviotto@gmail.com">ngraviotto@gmail.com</Link>
+        </p>
       </div>
 
       <div className="contact-RRSS">
         <h3 className="contact-text">RRSS</h3>
         <ul>
-          <li>Instagram</li>
-          <li>Linkedin</li>
-          <li>Github</li>
+          <li>
+            <Link
+              to="https://www.instagram.com/natachagraviotto_/"
+              target="_blank"
+            >
+              Instagram
+            </Link>
+          </li>
+
+          <li>
+            <Link
+              to="https://www.linkedin.com/in/natacha-graviotto-lled%C3%B3-570564161/"
+              target="_blank"
+            >
+              Linkedin
+            </Link>
+          </li>
+          <li>
+            <Link to="https://github.com/natachagraviotto/" target="_blank">
+              Github
+            </Link>
+          </li>
         </ul>
       </div>
     </section>
